75a105aceca94095a6e059421687b7b174cd3439
Emilio <eramirezc145@gmail.com>
2025-11-20 09:06:40 -0600

    Command declarations

--- src/main/Commands/CommandManager.h
+++ src/main/Commands/CommandManager.h
@@ -10,9 +10,21 @@ public:
 
 CommandManager(ArmSubsystem *arm, ElevatorSubsystem *elevator, GrabberSubsystem *grabber, IntakeSubsystem *intake);
 
-
-frc2::CommandPtr MoveElevator();
-frc2::CommandPtr MoveElevatorAndArm();
+frc2::CommandPtr L1();
+frc2::CommandPtr L2();
+frc2::CommandPtr L3();
+frc2::CommandPtr L4();
+frc2::CommandPtr L1Confirm();
+frc2::CommandPtr L2Confirm();
+frc2::CommandPtr L3Confrim();
+frc2::CommandPtr L4Confirm();
+frc2::CommandPtr holdCoral();
+frc2::CommandPtr Sustain();
+frc2::CommandPtr Processor();
+frc2::CommandPtr ProcessorConfirm();
+frc2::CommandPtr Intake();
+frc2::CommandPtr twoPiece();
+frc2::CommandPtr Initial();
 
 
 private:
